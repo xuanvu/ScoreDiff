b0e75b42bd4c090333e2e98ee19b0c766408fd62
Julien Dubeau <jdubeau@dons.usfca.edu>
2012-07-20 21:32:59 -0700

    added a new test

--- src/test.py
+++ src/test.py
@@ -221,6 +221,9 @@ def test_accidentals(score1, score2, measure = 0, part = 0):
 	   >>> test_accidentals('scriabin_opus_8_no2.mxl', 'ravel_sonatine_1.mxl')
 	   False
 
+	   >>> test_accidentals('bwv66.6.mxl', 'different_accidentals4.mxl')
+	   False
+
 
 	  
 	"""
